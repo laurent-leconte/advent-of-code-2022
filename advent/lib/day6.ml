a0d3cb2782c7a8cb25df88764d4a984c66178059
Laurent Leconte <laurent@lau.rent>
2022-12-07 13:34:13 +0100

    Day 6, part 2

--- advent/lib/day6.ml
+++ advent/lib/day6.ml
@@ -1,14 +1,27 @@
-let all_different a b c d =
-  a <> b && a <> c && a <> d && b <> c && b <> d && c <> d
+module SS = Set.Make(String)
 
-let rec find_offset i = function
-  | a::(b::c::d::_ as remainder) -> if all_different a b c d then i + 4 else find_offset (i+1) remainder
-  | _ -> failwith "Start packet not found"
+let all_different lst =
+  let n = List.length lst in
+  let set = List.fold_right SS.add lst SS.empty in
+  SS.cardinal set = n 
 
-  let part1 input = 
+
+let find_offset window_size lst =
+  let window, tl = Utils.take window_size lst in
+  let rec aux window offset = function
+  | a::tl -> if all_different window 
+             then 
+               offset + window_size
+             else 
+               let new_window = (List.tl window)@[a] in
+               aux new_window (offset + 1) tl
+  | _ -> failwith "Start packet not found" in
+  aux window 0 (tl@["."]) (* add a railguard char at the end of the input to cat*)
+
+  let generic size input = 
     Utils.read_lines input
     |> List.hd
     |> Utils.explode
-    |> find_offset 0
+    |> find_offset size
 
-  let both_parts input = (part1 input, 0)
+  let both_parts input = (generic 4 input, generic 14 input)
