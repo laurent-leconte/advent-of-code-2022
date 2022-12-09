080a7d1bc7fd526af1ab06e93b2c2ca87dc04ecd
Laurent Leconte <laurent@lau.rent>
2022-12-09 17:45:02 +0100

    Day 9, generic solution

--- advent/lib/day9.ml
+++ advent/lib/day9.ml
@@ -8,6 +8,8 @@ let add (x1, y1) (x2, y2) = (x1 + x2), (y1 + y2)
 
 let sub (x1, y1) (x2, y2) = (x1 - x2), (y1 - y2)
 
+let sign x = compare x 0
+
 (** Takes the vector between H and T (i.e. H-T) and outputs the movement for T *)
 let move_tail = function
   (* same row / column *)
@@ -18,11 +20,7 @@ let move_tail = function
   | -2,0 -> -1,0
   (* diagonals *)
   | 1,1 | 1,-1 | -1,1 | -1,-1 -> 0,0
-  | 2,1 | 1,2 -> 1,1
-  | 2,-1 | 1,-2 -> 1,-1
-  | -2,1 | -1,2 -> -1,1
-  | -2,-1 | -1,-2 -> -1,-1
-  | x,y -> failwith @@ Printf.sprintf "Incorrect delta %d,%d" x y
+  | x,y -> (* at this stage abs(x) + abas(y) > 3 *) sign x, sign y
 
   let move_head (x,y) = function
     | "U" -> x, y+1
@@ -32,22 +30,25 @@ let move_tail = function
     | _   -> failwith "unknown direction"
 
 let str_of_point (x,y) = (string_of_int x) ^ "," ^ (string_of_int y)
+let str_of_rope r = "[" ^ (String.concat ";" (List.map str_of_point r)) ^ "]"
 
-let one_step head tail dir =
-  Printf.printf "Before: head=%s; tail=%s; dir=%s\n" (str_of_point head) (str_of_point tail) dir;
-  let new_head = move_head head dir in
-  let delta = sub new_head tail in
-  Printf.printf "After moving head: head=%s; delta=%s\n" (str_of_point new_head) (str_of_point delta);
-  let new_tail = add tail (move_tail delta) in
-  Printf.printf "After moving tail: head=%s; tail=%s\n" (str_of_point new_head) (str_of_point new_tail);
-  new_head, new_tail
+let one_step rope dir =
+  (* Printf.printf "Before: %s\n" (str_of_rope rope); *)
+  let rec move_rope leader acc = function
+    | follower::tl -> let delta = sub leader follower in
+                let new_follower = add follower (move_tail delta) in
+                move_rope new_follower (leader::acc) tl
+    | [] -> List.rev (leader::acc), leader in
+  let new_rope, new_tail = move_rope (move_head (List.hd rope) dir) [] (List.tl rope) in
+  (* Printf.printf "After: %s\n" (str_of_rope new_rope); *)
+  new_rope, new_tail
 
-let rec all_movements points head tail = function
-  | (dir,s)::tl -> let new_head, new_tail = one_step head tail dir in
+let rec all_movements points rope = function
+  | (dir,s)::tl -> let new_rope, new_tail = one_step rope dir in
                    let new_points = Points.add new_tail points in
                    if s = 1 
-                    then all_movements new_points new_head new_tail tl
-                    else all_movements new_points new_head new_tail ((dir, s-1)::tl)
+                    then all_movements new_points new_rope tl
+                    else all_movements new_points new_rope ((dir, s-1)::tl)
   | [] -> points
 
 let parse_line l =
@@ -55,13 +56,15 @@ let parse_line l =
     | [dir;step] -> (dir, int_of_string step)
     | _ -> failwith "Wrong line format"
 
-let part1 input =
-  let head = 0,0 in
-  let tail = 0,0 in
-  let points = Points.singleton tail in
+let generic rope_size input =
+  let rec make_rope acc = function
+    | 0 -> acc
+    | n -> make_rope ((0,0)::acc) (n-1) in
+  let rope = make_rope [] rope_size in
+  let points = Points.singleton (0,0) in
   Utils.read_lines input
   |> List.map parse_line
-  |> all_movements points head tail
+  |> all_movements points rope
   |> Points.cardinal
 
-let both_parts input = (string_of_int (part1 input), "")
+let both_parts input = (string_of_int (generic 2 input), string_of_int (generic 10 input))
